ce9b895595063f1ca9ac2212774edcb2ac6f10d8
Yunosuke Yamada <39757050+YunosukeY@users.noreply.github.com>
2024-11-08 23:58:57 +0900

    feat: logging (#35)
    * cargo add
    
    * add log

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,7 @@
+#![allow(dead_code)]
+
+use tracing::Level;
+
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub const BOOL_BYTES: i32 = 1;
@@ -17,4 +21,16 @@ pub fn current_time_millis() -> u128 {
 pub fn waiting_too_long(start_time: u128) -> bool {
     let current_time = current_time_millis();
     current_time - start_time > MAX_WAIT_TIME_MILLIS
+}
+
+pub fn init_log() {
+    tracing_subscriber::fmt()
+        .json()
+        .with_file(true)
+        .with_line_number(true)
+        .with_max_level(Level::INFO)
+        .with_level(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .init();
 }
